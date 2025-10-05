388acb093df8f76fecaa4dd21fa160cbcb21878a
gusgus <73510218+AgusMessina@users.noreply.github.com>
2025-10-05 11:29:53 -0300

    Ahora el grafico esta listo | Mejor diccionario

--- 3d.py
+++ 3d.py
@@ -3,23 +3,21 @@ import numpy as np
 import requests
 import random
 
-# Paso 1: Obtener datos de la NASA NeoWs API
-
-API_KEY = 'm6W81jDrD2TMVWarINf6yCbvesjH82cCUeGBFb41'
-asteroid_id = '3727639'
+API_KEY = 'm6W81jDrD2TMVWarINf6yCbvesjH82cCUeGBFb41'        #Tenemos la API KEY de los objetos cercanos a la tierra
+asteroid_id = '3727639'                                     #De aqui recuperamos todo 
 
 url = f'https://api.nasa.gov/neo/rest/v1/neo/{asteroid_id}?api_key={API_KEY}'
 response = requests.get(url)
-data = response.json()
+data = response.json()                                      #"Data" recupera los datos de la api
 nombre_asteroide = data['name']
 
 # Paso 2: Extraer diámetro estimado
-diameter = data['estimated_diameter']['meters']['estimated_diameter_max']
-x = random.randint(1,100)
+diameter = data['estimated_diameter']['meters']['estimated_diameter_max']   #El diametro es el estimado, representado en metros
+
 # Paso 3: Generar puntos para simular la forma del meteorito
-u = np.linspace(0, np.pi, 15)
-v = np.linspace(0, np.pi, 15)
-u, v = np.meshgrid(u, v)
+u = np.linspace(0, 2 * random.uniform((np.pi - 1), np.pi + 1), 15)          #15 numeros el cual su distancia es uniforme, desde 0 hasta 2 * un numero aleatorio entre pi-1 y pi+1 
+v = np.linspace(0, 2 * random.uniform((np.pi - 1), np.pi + 1), 15)          #15 numeros el cual su distancia es uniforme, desde 0 hasta 2 * un numero aleatorio entre pi-1 y pi+1
+u, v = np.meshgrid(u, v)                                                    #Crea una malla a partir de los vectores de coordenadas u y v       
 
 # Usar el diámetro de la NASA para el radio base
 r = (diameter / 2) * (1 + 0.5 * np.random.rand(*u.shape))
@@ -28,14 +26,34 @@ y = r * np.sin(u) * np.sin(v)
 z = r * np.cos(v)
 
 # Crear la figura 3D
-fig = plt.figure(figsize=(8, 8))
+fig = plt.figure(figsize=(7,6))
 ax = fig.add_subplot(111, projection='3d')
 
-ax.plot_surface(x, y, z, color='grey', alpha=0.75, shade=True)
+ax.plot_surface(x, y, z, color='grey', alpha=0.75, shade=True)              #El meteorito es de color gris, con una transparencia del 75% y sombreado
 
+ax.set_facecolor('black')                                                   #El fondo del 3D se pone en color negro
+ax.grid(True, color='white', alpha=0.3)                                     #La grilla es de color blanco, algo transparente
+fig.patch.set_facecolor('black')                                            #El fondo de la ventana se pone en color negro
+
+
+#Etiquetamos los respectivos ejes, representados en metros
 ax.set_xlabel('X (Meters)')
 ax.set_ylabel('Y (Meters)')
 ax.set_zlabel('Z (Meters)')
-ax.set_title(f"Meteorito 3D (ID: {asteroid_id}, Nombre: {nombre_asteroide}")
+
+#Color blanco la etiqueta de cada eje
+ax.xaxis.label.set_color('white')
+ax.yaxis.label.set_color('white')
+ax.zaxis.label.set_color('white')
+
+#Color blanco para la escala
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', colors='white')
+ax.tick_params(axis='z', colors='white')
+
+ax.set_title(f"Meteorito 3D \n (ID: {asteroid_id}, Nombre: {nombre_asteroide}")
+ax.title.set_color('white')
+
+plt.get_current_fig_manager().toolbar.pack_forget()                         # Oculta la toolbar
 
 plt.show()
--- lista_Asteroides.py
+++ lista_Asteroides.py
@@ -7,21 +7,21 @@ diccionarioAsteroides = {}
 API_KEY = 'm6W81jDrD2TMVWarINf6yCbvesjH82cCUeGBFb41'       #API KEY de la NASA
 
 
-fechaAux = datetime.timedelta(days=7)       #fecha de hace 7 dias
+start_date = datetime.timedelta(days=7)       #fecha de hace 7 dias
 end_date = datetime.date.today()            #fecha actual que sirve lesgo
 
-start_date = end_date - fechaAux
 
-
-urlBusqueda = f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={API_KEY}'
+urlBusqueda = f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={API_KEY}'        #Busqueda de meteoritos desde "start_date" hasta "end_date"
 response = requests.get(urlBusqueda)
 data = response.json()
 
-for fecha in data['near_earth_objects']:
-    for asteroide in data['near_earth_objects'][fecha]:
-        asteroideId = asteroide['id']
-        nombre_asteroide = asteroide['name']
-        diccionarioAsteroides[nombre_asteroide] = asteroideId
 
-stringAux = input("Nombre del asteroide: ")
-print(diccionarioAsteroides.get(stringAux, "Asteroide no encontrado"))
+for fecha in data['near_earth_objects']:                    #Recuperamos las fechas
+    for asteroide in data['near_earth_objects'][fecha]:     #Recuperamos los datos de los asteroides
+        asteroideId = asteroide['id']                       #Recuperamos el ID del asteroide
+        nombre_asteroide = asteroide['name']                #Recuperamos el nombre de cada asteroide
+        diccionarioAsteroides[nombre_asteroide] = asteroideId       #Guardamos en el diccionario, los asteroides como ID, teniendo su llave los nombres
+        
+#print(diccionarioAsteroides.keys(),'\n')
+#stringAux = input("Nombre del asteroide: ")
+#print(diccionarioAsteroides.get(stringAux, "Asteroide no encontrado"))
